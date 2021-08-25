80259f9c28c16e76b1291a58164b77feb46ce7cd
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-08-25 12:36:32 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -570,12 +570,12 @@ allcmd = ["play", "current", "playlist", "song", f"song@{USERNAME}", f"play@{USE
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/free_music123"),
+                InlineKeyboardButton("GROUP", url="https://t.me/song_requestgroup"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("Movie Group", url="https://t.me/all_super_movies"),
             ]
          ]
-    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
+    k=await m.reply_photo(photo="https://telegra.ph/file/2cc60cb6ad087b6f14ed7.png", caption="**Sorry, You Can't Use This Bot In This Group!😉/n Contact @brandfreehelper to make a bot for your group**", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
